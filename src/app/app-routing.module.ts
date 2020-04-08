392f568dd5a49fe314bb11b2e00682b039f2213d
unknown <arinzeokorji@yahoo.com>
2020-04-08 10:18:42 +0100

    Modules created, header semantic and basic style done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'article', loadChildren: () => import('./article/article.module').then(m => m.ArticleModule) },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) },
+  { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) },
+  { path: 'editor', loadChildren: () => import('./editor/editor.module').then(m => m.EditorModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
